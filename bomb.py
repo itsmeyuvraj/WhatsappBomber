9fff0b829e0d2f822a05f585ee961c626250db74
Yuvraj Sharma <99718182+yuvrajpoudyal@users.noreply.github.com>
2022-03-17 12:32:17 +0530

    Added an emoji

--- bomb.py
+++ bomb.py
@@ -1,21 +1,14 @@
 
 import random
-
 import pyautogui as pg
-
 import time
-
-
 
 animal = ('Dog', 'Monkey', 'Cow', 'Buffalow' , 'Elephant', 'Donkey', 'Cat' )
 
-time.sleep(10)
-
 
 for i in range (1,1000):
-
     a  = random.choice(animal)
-
     pg.write('You Are a ' + a)
-
+    pg.write('U+1F47F')  #emoji 
     pg.press('enter')
+    time.sleep(10)
